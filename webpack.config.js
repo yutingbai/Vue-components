716552b568776c1c556d144637ed73e1c3a86236
yutingbai <baiyuting_i@didiglobal.com>
2020-02-11 01:00:16 +0800

    test code

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 var path = require('path')
 var webpack = require('webpack')
+// 执行环境
 const NODE_ENV = process.env.NODE_ENV;
 console.log("-----NODE_ENV===",NODE_ENV);
+
 module.exports = {
   // 根据不同的执行环境配置不同的入口
   entry: NODE_ENV == 'development' ? './src/main.js' : './src/components/messageList/index.js',
