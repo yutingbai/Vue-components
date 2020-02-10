77efaadcaa16b1d00901313d4a26a0964fb425c9
yutingbai <baiyuting_i@didiglobal.com>
2020-02-10 22:59:01 +0800

    test code

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,28 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <message-List :num1="num1" :num2="num2" v-on:getSumFromChild="receiveChildSum"></message-List>
+    
+    <p>从子组件获取到的值：{{sumFromChild}}</p>
   </div>
 </template>
 
 <script>
+import messageList from '../src/components/messageList/messageList'; // 引入
 export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      num1: 4,
+      num2: 5,
+      sumFromChild:0,
+    }
+  },
+  components:{ //注册插件
+    messageList
+  },
+  methods:{
+    receiveChildSum(sum){ //自定义事件，接收子组件的和
+      this.sumFromChild = sum;
     }
   }
 }
@@ -38,23 +36,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
 }
 </style>
